138390d6308943160e9d22c71dcb76faaa359f64
Joel Feitosa <admjoelfeitosadasilva@gmail.com>
2026-04-21 23:00:13 -0300

    feat(movies): add validation middleware

---
+++ src/middlewares/movieValidation.ts
@@ -0,0 +1,20 @@
+import type { RequestHandler} from 'express';
+
+export const validateMovie: RequestHandler = (req, res, next) => {
+  const { title, genre, releaseYear, directorId, description } = req.body;
+
+  if (!title || !genre || !releaseYear || !directorId) {
+    return res.status(400).json({ error: 'title, genre, releaseYear and directorId are required' });
+  }
+
+  if (description && description.length > 255) {
+    return res.status(400).json({ error: 'desccription must be at most 255 characters' });
+  }
+
+  const currentYear = new Date().getFullYear();
+  if (typeof releaseYear !== 'number' || releaseYear > currentYear) {
+    return res.status(400).json({ error: 'releaseYear must be a valid number and not in the future'})
+  }
+
+  next();
+};
